23df7a4a12f5bafc2b99b9686a5917728c4d9cb1
italojohnny <italojohnnydosanjos@gmail.com>
2024-11-16 07:48:55 -0300

    adiciona comando verifica ao cli

--- dados_publicos_cnpj/__main__.py
+++ dados_publicos_cnpj/__main__.py
@@ -1,4 +1,9 @@
 import aiohttp
+import asyncio
+import click
+
+
+URL = "https://arquivos.receitafederal.gov.br/dados/cnpj/dados_abertos_cnpj/"
 
 
 async def check_url_availability(url):
@@ -9,3 +14,24 @@ async def check_url_availability(url):
 
         except aiohttp.ClientError:
             return False
+
+
+@click.group()
+def cli_commands():
+    pass
+
+
+@click.command()
+def verifica():
+    click.echo(f"Verificando o site: {URL}")
+    availability = asyncio.run(check_url_availability(URL))
+    if availability:
+        click.secho("O site está online!", fg='green')
+        exit(0)
+    else:
+        click.secho("O site está offline ou inacessível.", fg='red')
+        exit(1)
+
+
+cli_commands.add_command(verifica)
+cli_commands()
